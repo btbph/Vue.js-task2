e5da017c5fd6d4720b4ab4b080ed0684b2629772
Mikhail Ulyanov <m.ulyanov96@mail.ru>
2018-09-13 17:47:59 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ShowName v-for="(name, index) in nameArr" :key="index" :name="name"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      nameArr: ['Kate', 'Max', 'John', 'Lisa', 'Anna', 'Bob', 'Nick', 'Conor', 'Bill']
+    }
+  }
+}
+</script>
